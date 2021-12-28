599a2fbf60a640756277c90fcbac11b6f25bf5f0
Nandadewanta <94246636+Nandadewanta@users.noreply.github.com>
2021-12-29 04:31:03 +0700

    Update infokyura.js

--- infokyura.js
+++ infokyura.js
@@ -1,29 +1,29 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
 ╭─˗ˏˋ *Informasi Owner* ´ˎ˗
-│ ✎ Nama : Kyura 
-│    Kelas : 11 DKV 2,SMK N 1 SAWAN
-│    Umur : 16
-│    Asal : Bali 
+│ ✎ Nama : Dewanta
+│    Kelas : lulus
+│    Umur : 20
+│    Asal : jateng 
 │    Status : gatau mw isi apa anj
 │    Suka : Jepang, Coding (dikit), B.Inggris
 │ ✎ _Official Grup_ :
-│    bit.ly/officialgrupkyurabot
+│    bit.ly/off
 │ ✎ _Instagram_ :
-│    bit.ly/instagramkyura
-│ ✎ _YouTube_ : 
-│    bit.ly/Youtubekyura
+│
+│ 
+
 │ ✎ _FaceBook_ :
-│    bit.ly/facebookkyura
+│   
 │ ✎ _WhatsApp_ :
-│    bit.ly/whatsappkyura
+│   
 ╰‿‿‿‿‿‿‿‿
 `.trim(), m)
 }
 
-handler.help = ['infokyura']
+handler.help = ['infoDewanta']
 handler.tags = ['main']
-handler.command = /^(infokyura)$/i
+handler.command = /^(infoDewanta)$/i
 
 handler.exp = 150
 
